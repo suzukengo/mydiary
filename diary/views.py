e186dc7a0a0c122f1d88ae252a2d7644f15392fd
suzukengo <k10212000@gmail.com>
2022-09-14 16:06:16 +0900

    削除、更新の機能の追加

--- diary/views.py
+++ diary/views.py
@@ -1,9 +1,17 @@
+from datetime import timezone
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic import CreateView
-
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from .models import Diary
 from .forms import DiaryForm
+
+
 
 # Create your views here.
 #TemplateViewはDjango特有のクラスを継承している。Viewを作成するための汎用的なビュー。汎用的な処理を返せるView
@@ -20,3 +28,29 @@ class DiaryCreateView(CreateView):
 
 class DiaryCreateCompleteView(TemplateView):
     template_name='diary_create_complete.html'
+
+class DiaryListView(ListView):
+    template_name='diary_list.html'
+    model=Diary
+
+#DetailViewはデータの詳細画面を作成するために便利なクラス
+class DiaryDetailView(DetailView):
+    template_name='diary_detail.html'
+    model=Diary
+
+class DiaryUpdateView(UpdateView):
+    template_name='diary_update.html'
+    model=Diary
+    fields=('date','title','text')
+    success_url=reverse_lazy('diary:diary_list')
+
+    def form_valid(selfself,form):
+        diary=form.save(commit=False)
+        diary.update_at=timezone.now()
+        diary.save()
+        return super().form_valid(form)
+
+class DiaryDeleteView(DeleteView):
+    template_name='diary_delete.html'
+    model=Diary
+    success_url=reverse_lazy('diary:diary_list')
